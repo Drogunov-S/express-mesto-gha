bf63b1624fb0af8a3ace2a3cf25ef770f9d50693
sergey <drogunov-s@yandex.ru>
2023-06-25 23:37:32 +0300

    first test

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-module.exports.PORT = process.env.PORT || 3001;
+module.exports.PORT = process.env.PORT || 3000;
 module.exports.COOKIE_LIAVE_TIME = 3600000 * 24 * 7;
 module.exports.HTTP_ONLY = true;
 module.exports.JWT_SECRET = '9e4182a7216d6400412b6e201e049d1590d63b8531f00921f484597beead8b0c';
